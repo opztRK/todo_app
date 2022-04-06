b8ffbdf4379c3a16096c9147e829be3c01593cbf
ryuki <ryukik1020@gmail.com>
2022-04-06 20:32:50 +0900

    4月6日(水)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,9 +22,11 @@ use Illuminate\Support\Facades\Route;
 //初期→Route::get('/','TaskController@top');
 
 //会員登録TOP
-Route::get('/','HomeController@index')->name('home');
-//認証メソッド
 Auth::routes(); //会員登録・ログイン・ログアウト・パス再設定の各機能で必要なルーティングすべてを定義
+
+Route::get('/','HomeController@index')->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//認証メソッド
 
 //インデックス
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
@@ -36,3 +39,5 @@ Route::post('/folders/{id}/tasks/create','TaskController@create');
 //編集機能
 Route::get('/folders/{id}/tasks/{task_id}/edit','TaskController@showEditForm')->name('tasks.edit');
 Route::post('/folders/{id}/tasks/{task_id}/edit','TaskController@edit');
+
+
